68a31c2b71857fae55c875236b37e4e2152f6e55
aracelychavez <aracely.chavez@gmail.com>
2024-07-22 19:56:15 -0600

    inicio

---
+++ helloworld.py
@@ -0,0 +1,6 @@
+print("This line will be printed.")
+name = input("What is your name: ")
+firstname = input("What is your firstname: ")
+city = input("Where do you live: ")
+
+print(firstname, name, "lives in", city)
